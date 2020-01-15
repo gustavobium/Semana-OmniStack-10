3fe3a6341d8321bb1be499c6c1e10d355ac5b270
gustavobium <37386964+gustavobium@users.noreply.github.com>
2020-01-14 23:51:43 -0300

    pequena medida de segurança
    adicionado helmet para esconder qual o sistema de backend utilizado (no caso node.js)

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const helmet = require('helmet');
 
 const app = express();
+
+// Aqui é uma medida de segurança básica apenas para começar a pensar no assunto, ele remove do navegador onde fala em qual linguagem o backend é desenvolvido.
+//  ler https://expressjs.com/pt-br/advanced/best-practice-security.html
+app.use(helmet());
+app.disable('X-Powered-By');
+
 mongoose.connect('mongodb+srv://gdonadon:gu@1993@cluster0-tezm2.gcp.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
